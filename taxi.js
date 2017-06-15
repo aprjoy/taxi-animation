e07d5b4e7c51dad79a44df931203e92151da7760
aprjoy <anjoyner@gmail.com>
2017-06-15 14:34:39 -0400

    changed animation to toggle function
    changed order of html and added css so not to have to hide after image, changed js to fadeToggle function

--- taxi.js
+++ taxi.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
 
-	//make sure after image isn't visible on page
-	$(".after").hide();
-
 	//when image is clicked
  	$("img").click(function(){
- 		//fade out before image currently on screen
- 		$(".before").fadeOut(1000);
- 		//gradually show after image
- 		$(".after").fadeIn(1000);
-
- 	});
-    
+ 		//fade in and out before and after images
+ 		$(".before").fadeToggle(1000);
+ 	});  
 });
