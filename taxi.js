a3d2048aed3dc1f6b702deb1fb28c8a089e4accb
aprjoy <anjoyner@gmail.com>
2017-06-15 15:01:46 -0400

    changed fade in and out time

--- taxi.js
+++ taxi.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 	//when image is clicked
  	$("img").click(function(){
  		//fade in and out before and after images
- 		$(".before").fadeToggle(1000);
+ 		$(".before").fadeToggle();
  	});  
 });
