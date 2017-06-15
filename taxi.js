57a4376df4e0e411a09eb52557ca1407f31226b7
aprjoy <anjoyner@gmail.com>
2017-06-15 14:29:02 -0400

    add comments, slowed animation
    changed time parameter for fade out and fade in to 1000 ms and added explanatory comments

--- taxi.js
+++ taxi.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
+	//make sure after image isn't visible on page
 	$(".after").hide();
 
+	//when image is clicked
  	$("img").click(function(){
-
- 		$(".before").fadeOut();
- 		$(".after").fadeIn();
+ 		//fade out before image currently on screen
+ 		$(".before").fadeOut(1000);
+ 		//gradually show after image
+ 		$(".after").fadeIn(1000);
 
  	});
     
